aa27c283ec87734abff799deb4c52a81a6a481c9
Remy Adriaanse <remy@envase.io>
2021-05-11 16:31:45 +0200

    churn(deps): bump Go and Bingo version
    Go 1.16 allows us to compile for Apple Silicon (M1) and bingo needs to
    be updated for compatiblity reasons.

--- .bingo/Variables.mk
+++ .bingo/Variables.mk
@@ -17,11 +17,11 @@ GO     ?= $(shell which go)
 #	@echo "Running bingo"
 #	@$(BINGO) <flags/args..>
 #
-BINGO := $(GOBIN)/bingo-v0.3.1
+BINGO := $(GOBIN)/bingo-v0.4.0
 $(BINGO): $(BINGO_DIR)/bingo.mod
 	@# Install binary/ries using Go 1.14+ build command. This is using bwplotka/bingo-controlled, separate go module with pinned dependencies.
-	@echo "(re)installing $(GOBIN)/bingo-v0.3.1"
-	@cd $(BINGO_DIR) && $(GO) build -mod=mod -modfile=bingo.mod -o=$(GOBIN)/bingo-v0.3.1 "github.com/bwplotka/bingo"
+	@echo "(re)installing $(GOBIN)/bingo-v0.4.0"
+	@cd $(BINGO_DIR) && $(GO) build -mod=mod -modfile=bingo.mod -o=$(GOBIN)/bingo-v0.4.0 "github.com/bwplotka/bingo"
 
 GOLANGCI_LINT := $(GOBIN)/golangci-lint-v1.38.0
 $(GOLANGCI_LINT): $(BINGO_DIR)/golangci-lint.mod
